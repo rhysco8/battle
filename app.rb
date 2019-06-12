0ce9433709bc9fb553d2e46ab2f14a5ea4bd4944
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-06-12 17:21:47 +0100

    passes names as param to /names

--- app.rb
+++ app.rb
@@ -6,5 +6,11 @@ class BattleApp < Sinatra::Base
     erb :index
   end
 
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
+
   run! if app_file == $0
 end

29793fedd5fb5849b7c5d1d4b8aa420babdb8be4
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-06-12 18:03:23 +0100

    enables sessions

--- spec/features/battle_spec.rb
+++ spec/features/battle_spec.rb
@@ -2,21 +2,13 @@
 # So we can play a personalised game of Battle,
 # We want to Start a fight by entering our Names and seeing them
 
-feature "asks for player names" do
-  scenario "fill out Player 1 name form" do
+feature "Enter player names" do
+  scenario "fill out player name forms" do
     visit "/"
     fill_in :player_1_name, with: 'Ms Jackson'
     fill_in :player_2_name, with: 'Godzilla'
 
     click_button('Submit')
-    expect(page).to have_content('Ms Jackson')
-  end
-  scenario "fill out Player 2 name form" do
-    visit "/"
-    fill_in :player_1_name, with: 'Ms Jackson'
-    fill_in :player_2_name, with: 'Godzilla'
-
-    click_button('Submit')
-    expect(page).to have_content('Godzilla')
+    expect(page).to have_content('Ms Jackson vs. Godzilla')
   end
 end
